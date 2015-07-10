f5cb5dd53e0c947f3f6c3d1ea09807c4f755fcda
Matt Zumwalt <matt@curationexperts.com>
2015-07-10 11:27:46 -0700

    comment out web-console in test_app

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -7,6 +7,11 @@ class TestAppGenerator < Rails::Generators::Base
     generate 'curation_concerns:install'
   end
 
+  def comment_out_web_console
+    gsub_file "Gemfile",
+              "gem 'web-console'", "# gem 'web-console'"
+  end
+
   def run_migrations
     rake "db:migrate"
   end
